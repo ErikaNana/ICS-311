6ebcafb7db0e9644261a4f88511a1f4facf36ee8
Erika Nana <enana@hawaii.edu>
2013-10-12 02:11:47 -1000

    Working version of Sorted DLL

--- Project 1/src/dll/DLinkedList.java
+++ Project 1/src/dll/DLinkedList.java
@@ -1,124 +1,166 @@
 package dll;
-/* Constructs a doubly-linked list. 
+/* Constructs a sorted doubly-linked list. 
  * @author Erika Nana
  */
 public class DLinkedList<Type> {
+	
 	private DNode<Type> head = null;
 	private int size = 0;
+	private static final int GREATER = 1;
+	private static final int LESSER = 2;
+	private static final int EQUAL = 3;
 	
-	 /**adds an item to the end of the list
-	  * */
-	public void add (Type value){
-
+	/* Inserts a value into the list while preserving sorted order*/
+	public void insert (Type value) {
+		DNode<Type> temp = head;
 		DNode<Type> newNode = new DNode<Type>(value);
-		if (this.isEmpty()) { 
+		boolean insert = false; //check to see if inserted
+		
+		if (size == 0) {
+			System.out.println("size 0");
 			head = newNode;
 		}
-		else {
-			DNode<Type> temp = head;
-			while (temp.getNext() != null) { 
-				temp = temp.getNext();
+		if (size == 1) {
+			System.out.println("size 1");
+			int compare = compareValue(value, head);
+			if (compare == GREATER || compare == EQUAL) {
+				//add the thing after
+				head.connectNext(newNode);
 			}
-			temp.setNext(newNode); 
-			newNode.setPrev(temp); 
+			else { //add the thing before
+				System.out.println("less than in size 1");
+				head = newNode;
+				head.connectNext(temp);
+			}
+		}
+		else {
+			//compare the values
+			if (size  > 1) {
+				System.out.println("bigger than 1");
+				while (temp.getNext()!= null) {
+					System.out.println("size:  " + size);
+					System.out.println("temp is:  " + temp.getValue());
+					int compare = compareValue(value,temp);
+					if (compare == LESSER) {
+						//special case if the head
+						if (temp == head) {
+							System.out.println("temp is the head");
+							head = newNode;
+							head.connectNext(temp);
+							temp.connectPrev(head);
+						}
+						else { //connect it before the node
+							temp.getPrev().connectNext(newNode);
+							temp.connectPrev(newNode);
+							newNode.connectNext(temp);
+						}
+						insert = true;
+						System.out.println("inserting before the lesser");
+						System.out.println("temp:  "+ temp.getValue());
+						break;	
+					}
+					temp = temp.getNext();
+				}
+				//reached the end of the list
+				if (!insert) {
+					System.out.println("end of the list");
+					//check the last node
+					if (compareValue(value, temp) == LESSER) {
+						temp.getPrev().connectNext(newNode);
+						temp.connectPrev(newNode);
+						newNode.connectNext(temp);
+					}
+					else {
+						temp.connectNext(newNode);
+					}
+					System.out.println("temp:  " + temp.getValue());	
+				}
+			}
 		}
 		size++;
 	}
-	 /**inserts an item to the specified index
-	  * */
-	public void insert (Type value, int index) {
-		DNode<Type> newNode = new DNode<Type>(value);
-		if (index ==0) {
-			newNode.connectNext(head);
-			head = newNode;
-		}
-		else {
-			if (index >= size) {
-				add(value);
-				return;
-			}
-			DNode<Type> temp = head;
-			int i = 0;
-			for (;i < index && temp.getNext() != null; i++) {
-				temp = temp.getNext();
-			}
-			if (i == index) {
-				//newNode should be inserted before temp node
-				temp.getPrev().connectNext(newNode);
-				newNode.connectNext(temp);
-			}
-		}
-		size++;
-	}
-	 /**removes an item at a specified index.
-	  * */
-	public void remove (int index) throws NodeCountException {
-	    if ((index < 0) || (index > size)) {
-	        throw new NodeCountException ("invalid index " + index
-	            + ": only 1.." + size + " items are available");
-	      }
-
-		if (index == 0) {
-			head = head.getNext();
-		}
-		else {
-			DNode<Type> temp = head;
-			int i = 0;
-			for(; i < index && temp.getNext() != null; i++) {
-				if ((temp.getNext() == null) && (i == index)) {
-					temp.getPrev().setNext(null);
-					temp.getPrev().connectNext(null);
+	/* Removes the given node from the list */
+	public void remove (String node) {
+		DNode<Type> temp = head;
+		while (temp != null) {
+			if (temp.getValue().equals(node)) {
+				//adjust the pointers
+				if (temp == head) {
+					head = null;
 					break;
 				}
-				temp = temp.getNext();
+				temp.getPrev().connectNext(temp.getNext());
+				size--;
 			}
-			if (i == index) {
-				temp.getPrev().setNext(temp.getNext());
-				temp.getPrev().connectNext(temp.getNext());//set value of temp
-			}
+			temp = temp.getNext();
 		}
-		size--;
 	}
-	 /**traverses the list and looks for the specified String name
-	  * and returns the index.
-	  * */
-	public int traverse (String name) {
+
+/*	 Traverses the list and looks for the specified String name and
+	 * returns the index*/
+	public int traverseGetIndex (String name) {
 		int i = 0;
 		String value = "";
 		value = head.getValue().toString();
-		
 		if (value.equals(name)) {
 			return 0;
-			
 		}
 		else {
 			DNode<Type> temp = head;
-
+			
 			for(i = 1; i < size + 1 && temp.getNext() != null; i++) {
 				temp = temp.getNext();
 				value = temp.getValue().toString();
 				if (value.equals(name)) {
-					temp.getPrev().setNext(temp.getNext());
-					break;
+					return i;
 				}
 			}
 		}
-		return i;
+		return -1;
 	}
-	 /**checks to see if the list is empty.
-	  * */
+	/* Traverses the list and returns the specific node */
+	public DNode<Type> traverseGetNode (String value) {
+		DNode<Type> temp = head;
+		while (temp != null) {
+			//find the node
+			if (temp.getValue().equals(value)) {
+				return temp;
+			}
+			temp = temp.getNext();
+		}
+		return null;
+	}
+	/* Checks to see if the list is empty*/
 	public boolean isEmpty() {
 		return head == null;
 	}
-	 /**provides a String representation of the list.
-	  * */
+	/* Provides a String representation of the list*/
 	public String toString() {
 		DNode<Type> temp = head;
 		String answer = "";
 		while(temp != null) {
 			answer = answer + temp.getValue().toString()+ " ";
 			temp = temp.getNext();
+
 		}
 		return answer;
 	}
+	
+	/* Helper method that compares strings*/
+	public int compareValue(Type value, DNode<Type> node) {
+		System.out.println("COMPARE VALUE");
+		//assume that list is of String type
+		int compare = ((String) value).compareTo((String) node.getValue());
+		if (compare > 0) {
+			System.out.println("GREATER");
+			return GREATER;
+		}
+		else if (compare == 0) {
+			System.out.println("EQUAL");
+			return EQUAL;
+		}
+		System.out.println("LESSER");
+		return LESSER;
+	}
+	
 }
--- Project 1/src/dll/Main.java
+++ Project 1/src/dll/Main.java
@@ -3,10 +3,28 @@ package dll;
 public class Main {
 	public static void main (String [] args) {
 		DLinkedList<String> list = new DLinkedList<String>();
-		String[] listValues = {"bob","storm","erika","kelsie","bree","georgia"};
-		for (int i = 0; i < 5; i++) {
-			list.insert(listValues[i], i);
+		String [] listValues = new String [4];
+		listValues[0] = "bob";
+		listValues[1] = "apple";
+		listValues[2] = "dog";
+		listValues[3] = "cat";
+
+		for (int i = 0; i < listValues.length; i++) {
+			list.insert(listValues[i]);
+			System.out.println("inserting:  " + listValues[i]);
+			System.out.println("list now:  " + list);
+			System.out.println("");
 		}
+		
+		System.out.println(list.traverseGetIndex("a"));
+		System.out.println(list.traverseGetIndex("b"));
+		System.out.println(list.traverseGetIndex("c"));
+		System.out.println(list.traverseGetIndex("d"));
+		
+		list.remove("dog");
+		list.remove("cat");
+		list.remove("bob");
+		list.remove("apple");
 		System.out.println(list);
 	}
 }
