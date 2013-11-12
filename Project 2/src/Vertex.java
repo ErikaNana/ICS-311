3d177cd020b1d274c0b62a2b739b23d8b699210e
Erika Nana <enana@hawaii.edu>
2013-11-11 20:39:23 -1000

    Initial commit

---
+++ Project 2/src/Vertex.java
@@ -0,0 +1,18 @@
+
+public class Vertex {
+
+	private String key = null;
+	
+	public Vertex(String key) {
+		this.key = key;
+	}
+	
+	public String getValue() {
+		return key;
+	}
+	
+	//just in case
+	public void setValue(String key) {
+		this.key = key;
+	}
+}
