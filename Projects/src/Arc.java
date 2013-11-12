fe59335a90844000b196c70f3647e510f332c5b5
Erika Nana <enana@hawaii.edu>
2013-11-11 20:37:43 -1000

    Initial commit

---
+++ Projects/src/Arc.java
@@ -0,0 +1,19 @@
+
+public class Arc {
+
+	private Vertex startVertex = null;
+	private Vertex endVertex = null;
+	
+	public Arc(String start, String end) {
+		this.startVertex = new Vertex(start);
+		this.endVertex = new Vertex(end);
+	}
+	
+	public Vertex getStartVertex() {
+		return startVertex;
+	}
+	
+	public Vertex getEndVertex() {
+		return endVertex;
+	}
+}
