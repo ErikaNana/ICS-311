c8b4b94506adc565ca9517f67ac30ea92377da0b
Erika Nana <enana@hawaii.edu>
2013-12-08 17:53:19 -1000

    Add licensing info

--- Extra Credit/src/code/Heap.java
+++ Extra Credit/src/code/Heap.java
@@ -1,10 +1,39 @@
 package code;
-
+/*
+ * Copyright (c) 2013, Erika Nana
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Extra Credit nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Erika Nana ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Erika Nana BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/** 
+ * Implementation of heap sort
+ * 
+ * @author Erika Nana
+ * 
+ */
 
 import java.util.ArrayList;
 
-
-//this starts at 1 not 0
 public class Heap implements Sort {
 	ArrayList<String> array;
 	int largest;
@@ -40,20 +69,13 @@ public class Heap implements Sort {
 		int left = 2*index;
 		int right = 2*index + 1;
 		if ((left <= size) && (array.get(left-1).compareTo(array.get(index-1)) > 0)){
-/*		if ((left <= size) && (Utils.compare(array.get(left-1), array.get(index-1))) == 1){*/
-/*			System.out.println("left:  " + array.get(left-1));
-			System.out.println("index:  " + array.get(index-1));
-			System.out.println("    left is bigger than index");*/
 			largest = left;
 		}
 		else {
 			largest = index;
 		}
-		if ((right <= size) && (array.get(right -1).compareTo(array.get(largest-1)) > 0))
-/*		if ((right <= size) && (Utils.compare(array.get(right-1), array.get(largest-1))) == 1)*/{
+		if ((right <= size) && (array.get(right -1).compareTo(array.get(largest-1)) > 0)){
 			largest = right;
-/*			System.out.println("right is:  " + array.get(right-1));
-			System.out.println("    right is bigger than largest");*/
 		}
 		if (largest != index) {
 			String temp = array.get(index-1);
@@ -103,7 +125,6 @@ public class Heap implements Sort {
 		return array.get(array.size()-1);
 	}
 	
-	//unused method from interface
 	public void sort(int p, int r) {
 		this.buildMaxHeap();
 		int size = array.size();
